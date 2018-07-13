3383da22c866b996f7079ccebec08886541588d4
scheripa <scheripa@tibco.com>
2018-07-13 23:36:43 +0530

    First commit

---
+++ src/com/scheripa/solutions/ReverseNumber.java
@@ -0,0 +1,31 @@
+package com.scheripa.solutions;
+
+public class ReverseNumber {
+	
+	    public int reverse(int number ){
+	        
+	        int prev_num = 0 ;
+	        int rev_num = 0;
+	        while(number != 0)
+	        {
+	         int current = number % 10;
+	            rev_num  = rev_num * 10 + current;
+	            if((rev_num-current) / 10!= prev_num ){
+	                 return 0;
+	            }
+	            prev_num = rev_num;
+	            number = number /10;
+	        }
+
+	        return rev_num;
+	    }
+
+	     public static void main(String []args){
+	        ReverseNumber reverseNumber = new ReverseNumber();
+	         int result =  reverseNumber.reverse(12345);
+	         System.out.println("Reverse Number is ==="+result +"\n");
+
+	     }
+	}
+
+
